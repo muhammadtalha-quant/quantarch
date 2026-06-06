dc621aa8e2cd527992730b3f6250d01b224220e8
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-06 21:47:27 +0500

    ai commit: fix screenshot and screenrecording bugs

--- hypr/.config/hypr/modules/keybindings.lua
+++ hypr/.config/hypr/modules/keybindings.lua
@@ -175,11 +175,26 @@ local ipc = {
 }
 
 local utils = {
-    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.SHIFT, KEYS.ALPHABET.S)] =     {cmd = "grim -g $(slurp) -t png -l 6 ~/Pictures/Captures/screenshot-$(date -Iseconds | sed 's/T/-/; s/+05:00//').png",                                                              desc = "Screenshot Region"},
-    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.CTRL, KEYS.ALPHABET.S)] =      {cmd = "grim -t png -l 6 ~/Pictures/Captures/screenshot-$(date -Iseconds | sed 's/T/-/; s/+05:00//').png",                                                                         desc = "Screenshot Full Screen"},
-    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.SHIFT, KEYS.ALPHABET.R)] =     {cmd = "wl-screenrec -g $(slurp) --audio --audio-device $SPEAKER -f ~/Videos/Captures/screenrecording-$(date -Iseconds | sed 's/T/-/; s/+05:00//').png",                            desc = "Screenrecord Region"},
-    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.CTRL, KEYS.ALPHABET.R)] =      {cmd = "wl-screenrec --audio --audio-device $SPEAKER -f ~/Videos/Captures/screenrecording-$(date -Iseconds | sed 's/T/-/; s/+05:00//').png",                                       desc = "Screenrecord Full Screen"},
-    [KEYS.LOCKS.SCROLLLOCK] =                                                {cmd = "hyprpicker -a -f hex",             desc = "Pick Color"},
+    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.SHIFT, KEYS.ALPHABET.S)] = {
+        cmd = [[bash -c 'grim -g "$(slurp)" -t png -l 6 "$HOME/Pictures/Captures/screenshot-$(date -Iseconds | sed "s/T/-/;s/\+05:00//;s/:/-/g").png"']],
+        desc = "Screenshot Region"
+    },
+    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.CTRL, KEYS.ALPHABET.S)] = {
+        cmd = [[bash -c 'grim -t png -l 6 "$HOME/Pictures/Captures/screenshot-$(date -Iseconds | sed "s/T/-/;s/\+05:00//;s/:/-/g").png"']],
+        desc = "Screenshot Full Screen"
+    },
+    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.SHIFT, KEYS.ALPHABET.R)] = {
+        cmd = [[bash -c 'wl-screenrec -g "$(slurp)" --audio --audio-device "${SPEAKER:-default}" -f "$HOME/Videos/Captures/screenrecording-$(date -Iseconds | sed "s/T/-/;s/\+05:00//;s/:/-/g").mp4"']],
+        desc = "Screenrecord Region"
+    },
+    [chord(KEYS.MODIFIER.SUPER, KEYS.MODIFIER.CTRL, KEYS.ALPHABET.R)] = {
+        cmd = [[bash -c 'wl-screenrec --audio --audio-device "${SPEAKER:-default}" -f "$HOME/Videos/Captures/screenrecording-$(date -Iseconds | sed "s/T/-/;s/\+05:00//;s/:/-/g").mp4"']],
+        desc = "Screenrecord Full Screen"
+    },
+    [KEYS.LOCKS.SCROLLLOCK] = {
+        cmd = "hyprpicker -a -f hex",
+        desc = "Pick Color"
+    },
 }
 
 
