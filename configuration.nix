3cfb64637134063e32ec4ac84508af37ba7a7d9c
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-17 23:15:42 +0500

    flake init

---
+++ configuration.nix
@@ -0,0 +1,128 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  hardware.bluetooth.enable = true;
+
+  boot = {
+    #loader.systemd-boot.enable = true;
+    #loader.efi.canTouchEfiVariables = true;
+    loader.grub = {
+      enable = true;
+      device = "/dev/sda";
+      useOSProber = false;
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  networking = {
+    # My System Host Name
+    hostName = "quantnix";
+    # Enable Wifi Support
+    networkmanager.enable = true;
+    # Firewall Settings
+    firewall = {
+      allowedTCPPorts = [ 53317 ]; 
+      allowedUDPPorts = [ 53317 ];
+    };
+  };
+  
+  time.timeZone = "Asia/Karachi";
+
+  # Internationalisaton Settings
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services = {
+    # X Server Settings
+    xserver = {
+      enable = true; # Enable the X Server
+      # Keyboard settings for X Server
+      xkb = {  
+        layout = "us"; # Set Keyboard Layout to US
+        variant = "";  # default variant
+      };
+    };
+    # Enable SDDM
+    displayManager.sddm.enable = true;
+    # Enable KDE
+    desktopManager.plasma6.enable = true;
+    # Disable PulseAudio 
+    pulseaudio.enable = false;
+    # Pipewire Settings
+    pipewire = {
+      enable = true; # Enable Pipewire
+      alsa.enable = true; # Enable Alsa
+      alsa.support32Bit = true; # Enable 32bit support for alsa
+      pulse.enable = true; # Enable Pipewire Pulse
+    };
+    # Enable libinput
+    libinput.enable = true;
+    # Enable pcscd
+    pcscd.enable = true;
+  };
+
+
+  security.rtkit.enable = true;
+
+  programs = {
+	  hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+	  fish.enable = true;
+	  starship = {
+	  enable = true;
+	    presets = [ "gruvbox-rainbow" ];
+	  };
+    gnupg.agent = {
+      enable = true;
+      pinentryPackage = pkgs.pinentry-curses;
+      enableSSHSupport = true;
+    };
+  };
+  users.users.muhammadtalha = {
+    isNormalUser = true;
+    description = "Muhammad Talha";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+	    vscode
+    	localsend
+      google-chrome
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    newcomputermodern
+  ];
+
+  environment.systemPackages = with pkgs; [
+	  git
+	  lazygit
+	  gh
+    neovim
+    fastfetch
+	  kitty
+    nixd
+    alejandra
+  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes"];	
+  system.stateVersion = "26.05";
+}
