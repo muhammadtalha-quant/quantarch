c4dea5ed6b3feb21a3480155bacac44b27765a31
Muhammad Talha <88384602+muhammadtalha-quant@users.noreply.github.com>
2026-06-18 10:37:49 +0500

    Update configuration.nix to match current system

--- configuration.nix
+++ configuration.nix
@@ -89,18 +89,21 @@
       enableSSHSupport = true;
     };
   };
+  users.mutableUsers = false;
+  users.users.root.hashedPassword = "$y$j9T$hbguh04FZh1JSM8nYVXS0.$9yG.bzlFyYT2NcDEKwxPmZuyN1Cz91DMpyewyfQAyM5";
   users.users.muhammadtalha = {
     isNormalUser = true;
     description = "Muhammad Talha";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.fish;
+    hashedPassword = "$y$j9T$hbguh04FZh1JSM8nYVXS0.$9yG.bzlFyYT2NcDEKwxPmZuyN1Cz91DMpyewyfQAyM5";
     packages = with pkgs; [
 	    vscode
     	localsend
       google-chrome
     ];
   };
-
+  
   nixpkgs.config.allowUnfree = true;
 
   fonts.packages = with pkgs; [
