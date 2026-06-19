3b1581c20c2de45c82dfffd6779ce40c9a6a38b5
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-19 22:59:22 +0500

    add home manager

--- flake.nix
+++ flake.nix
@@ -3,13 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    homeManager.url = "github:nix-community/home-manager/master";
+    homeManager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs , ... } @inputs : {
+  outputs = { self, nixpkgs , homeManager, ... } @inputs : {
   nixosConfigurations.quantnix = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs; };
           modules = [
             ./configuration.nix
+            homeManager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.muhammadtalha = import ./home.nix;
+            }
           ];
       };
   };
