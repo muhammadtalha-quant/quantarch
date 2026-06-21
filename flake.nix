68d452001f74b6806050e0686e9f328cc23c94d4
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-21 14:42:59 +0500

    auto: nix flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake that manages my NixOS System";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     homeManager.url = "github:nix-community/home-manager/master";
     homeManager.inputs.nixpkgs.follows = "nixpkgs";
   };
