4a165269694dd72a8bc0cad6e103dfe28d3768c4
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-21 14:42:28 +0500

    add module nix.nix

---
+++ system/modules/nix.nix
@@ -0,0 +1,13 @@
+{...}: {
+  nix.settings.experimental-features = [ "nix-command" "flakes"];	
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  nix.optimise = {
+    automatic = true;
+    dates = "weekly";
+  };
+  nix.settings.auto-optimise-store = true;
+}
