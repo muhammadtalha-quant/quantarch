e6b601924854afe52bdbb9b80b29225ede7bfbba
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-22 22:51:56 +0500

    use uwsm by default

--- system/modules/programs.nix
+++ system/modules/programs.nix
@@ -2,11 +2,10 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    withUWSM = false;
+    withUWSM = true;
     portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
   programs.fish.enable = true;
-  #programs.starship.enable = true;
   programs.gnupg.agent = {
     enable = true;
     pinentryPackage = pkgs.pinentry-curses;
