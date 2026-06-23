4e1f00e207496a8bd0f17e38b5bede27d0048f40
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-23 15:33:40 +0500

    typo(fix): fix typos in kitty config

--- user/modules/kitty.nix
+++ user/modules/kitty.nix
@@ -9,8 +9,8 @@
     background_opacity = 0.8;
     confirm_os_window_close = 0;
     enable_audio_bell = false;
-    srollback = "never";
-    srollback_lines = 100000;
+    scrollback = "never";
+    scrollback_lines = 100000;
   };
   programs.kitty.themeFile =  "GruvboxMaterialDarkHard";
   programs.kitty.enableGitIntegration = true;
@@ -22,7 +22,7 @@
     "page_up" = "scroll_page_up";
     "page_down" = "scroll_page_down";
     "ctrl+shift+t" = "new_tab";
-    "ctrl+shift+q" = "clost_tab";
+    "ctrl+shift+q" = "close_tab";
     "ctrl+shift+left" = "previous_tab";
     "ctrl+shift+right" = "next_tab";
     "ctrl+shift+equal" = "change_font_size all +2.0";
