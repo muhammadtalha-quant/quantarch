65ac2f585cda2b490ed48f382edbede6d350d9b5
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-24 10:20:30 +0500

    add module noctalia.nix for noctalia shell v5

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  description = "A flake that manages my NixOS System";
+  description = "A monolithic single host flake that manages my complete NixOS System";
+
+  nixConfig = {
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -11,25 +16,29 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs , homeManager, disko, ... } @inputs : {
-  nixosConfigurations.quantnix = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./system/configuration.nix
-            homeManager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.muhammadtalha = import ./user/home.nix;
-                backupFileExtension = "bak";
-              };
-            }
-            disko.nixosModules.disko
-            ./system/disko.nix
-          ];
-      };
+    nixosConfigurations.quantnix = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./system/configuration.nix
+        homeManager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.muhammadtalha = import ./user/home.nix;
+            backupFileExtension = "bak";
+          };
+        }
+        disko.nixosModules.disko
+        ./system/disko.nix
+      ];
+    };
   };
 }
