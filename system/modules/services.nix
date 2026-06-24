ead7ce20a05b741ae23fb68333f2306e8fefe623
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-24 19:20:30 +0500

    update: disable xserver

--- system/modules/services.nix
+++ system/modules/services.nix
@@ -1,9 +1,4 @@
 {...}: {
-  services.xserver.enable = true;
-  services.xserver.xkb = {
-      layout = "us"; # Set Keyboard Layout to US
-      variant = "";  # default variant
-  };
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
   services.displayManager.ly.enable = true;
