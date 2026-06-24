636bc67db2853b34257508ebef361d5ba1be715c
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-24 10:21:50 +0500

    chore: cofiguration complete for disko.nix

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -10,21 +10,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/NIXOS";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/BOOT";
-      fsType = "vfat";
-      options = [ "fmask=0777" "dmask=0777" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-label/SWAP"; }
-    ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
