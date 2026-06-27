f7bec344fa1148628fd8e6aa0fc61a9d988dafcd
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-27 20:10:11 +0500

    add noctalia theme/colors to fastfetch

--- user/modules/fastfetch.nix
+++ user/modules/fastfetch.nix
@@ -1,4 +1,18 @@
-{...}: {
+{...}:
+let
+   gruvbox = {
+    primary            = "#b8bb26";
+    on_primary         = "#282828";
+    secondary          = "#fabd2f";
+    tertiary           = "#83a598";
+    error              = "#fb4934";
+    surface            = "#282828";
+    on_surface         = "#fbf1c7";
+    on_surface_variant = "#ebdbb2";
+    outline            = "#786f6b";
+  };
+in
+{
   programs.fastfetch.enable = true;
   programs.fastfetch.settings = {
     logo  = {
@@ -22,42 +36,42 @@
         type = "os";
         key = "   OS";
         format = "{3}";
-        keyColor = "red";
+        keyColor = "${gruvbox.error}";
       }
       {
         type = "kernel";
         key = "   Kernel";
         format = "{1} {2}";
-        keyColor = "red";    
+        keyColor = "${gruvbox.error}";    
       }
       {
         type = "packages";
         key =  "  󰏗 Packages";
-        keyColor =  "green";
+        keyColor =  "${gruvbox.primary}";
       }
       {
         type =  "display";
         key =  "  󰍹 Display";
         format =  "{1}x{2} @ {3}Hz [{7}]";
-        keyColor =  "green";
+        keyColor =  "${gruvbox.primary}";
       }
       {
         type = "terminal";
         key = "   Terminal";
         format = "{3} {6}";
-        keyColor = "yellow";
+        keyColor = "${gruvbox.secondary}";
       }
       {
         type = "shell";
         key = "   Shell";
         format = "{6} {4}";
-        keyColor = "yellow";
+        keyColor = "${gruvbox.secondary}";
       }
       {
         type = "wm";
         key = "  󱗃 WM";
         format = "{2}";
-        keyColor = "yellow";
+        keyColor = "${gruvbox.secondary}";
       }
       {
         type = "custom";
@@ -68,6 +82,7 @@
         type = "title";
         key = "  ";
         format = "{6} {7} {8}";
+	      keyColor = "${gruvbox.primary}";
       }
       {
         type = "custom";
@@ -77,31 +92,31 @@
         type = "cpu";
         format = "{1} @ {7}";
         key = "   CPU";
-        keyColor = "blue";
+        keyColor = "${gruvbox.tertiary}";
       }
       {
         type = "gpu";
         format = "{1} {2}";
         key = "  󰊴 GPU";
-        keyColor = "blue";
+        keyColor = "${gruvbox.tertiary}";
       }
       {
         type = "memory";
         key = "   Memory ";
         format = "{1} used out of {2}";
-        keyColor = "magenta";
+        keyColor = "${gruvbox.secondary}";
       }
       {
         type = "disk";
         key = "  󱦟 OS Age ";
         folders = "/";
-        keyColor = "red";
+        keyColor = "${gruvbox.error}";
         format = "{days} days";
       }
       {
         type = "uptime";
         key = "  󱫐 Uptime ";
-        keyColor = "red";
+        keyColor = "${gruvbox.error}";
       }
       {
         type = "custom";
