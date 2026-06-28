07cee7cccd411394b679d7684f2e0c67665e731f
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-28 10:14:47 +0500

    keep only 2 working generations

--- system/modules/nh.nix
+++ system/modules/nh.nix
@@ -6,7 +6,7 @@
     clean = {
       enable = true;
       dates = "Mon *-*-* 09:00:00";
-      extraArgs = "--keep 5 --keep-since 2d";
+      extraArgs = "--keep 2 --keep-since 2d";
     };
   };
 }
