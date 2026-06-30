1f057bd609e118e3565546e8da5486c447937eb6
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-06-30 15:41:22 +0500

    chore: reengineered noctalia config

--- user/modules/noctalia.nix
+++ user/modules/noctalia.nix
@@ -1,151 +1,321 @@
 {...}: 
+let
+  outputMonitor = "eDP-1";
+in
 {
   programs.noctalia =  {
     enable = true;
     settings = {
       shell = {
-        clipboard_history_max_entries = 20.0;
-        corner_radius_scale = 1.3500000201165676;
-        font_family = "JetBrainsMono Nerd Font Mono";
+        font_family = "JetBrainsMono NFM";
+        clipboard_auto_paste = "off";
+        clipboard_confirm_clear_history = false;
+        clipboard_image_action_command = "imv";
+        date_format = "%B %d, %Y";
+        middle_click_opens_widget_settings = false;
         polkit_agent = true;
-        telemetry_enabled = true;
-        animation.speed = 0.6500000096857548;
+        time_format = "{:%A}\\n{:%I:%M %p}";
+        launcher = {
+          categories = false;
+          sort_by_usage = false;
+        };
         panel = {
-          clipboard_placement = "attached";
-          launcher_categories = false;
-          launcher_placement = "attached";
-          session_placement = "centered";
-          transparency_mode = "glass";
+          borders = false;
+          clipboard_position = "auto";
+          session_placement = "floating";
+          wallpaper_placement = "floating";
         };
-        session = {
-          actions = [
-            {
-              action = "logout";
+        screenshot = {
+          directory = "~/Pictures/Screenshots";
+          pipe_command = "satty -f";
+          pipe_to_command = true;
+        };
+        session.actions = [
+          {
+            action = "lock";
+            countdown_seconds = 0.0;
+            enabled = true;
+            shortcut = "1";
+            variant = "default";
+          }
+          {
+            action = "logout";
+            countdown_seconds = 0.0;
+            enabled = true;
+            shortcut = "2";
+            variant = "default";
+          }
+          {
+            action = "reboot";
+            countdown_seconds = 0.0;
+            enabled = true;
+            shortcut = "3";
+            variant = "default";
+          }
+          {
+            action = "shutdown";
+            countdown_seconds = 0.0;
+            enabled = true;
+            shortcut = "4";
+            variant = "destructive";
+          }
+        ];
+      };
+      idle = {
+        behavior_order = [ "lock" "screenoff" "sleep" ];
+        pre_action_fade_seconds = 0;
+        behavior = {
+          lock = {
+            action = "lock";
+            enabled = true;
+            timeout = 600.0;
+          };
+          screenoff = {
+            action = "screen_off";
+            enabled = true;
+            timeout = 660.0;
+          };
+          sleep = {
+              action = "lock_and_suspend";
               enabled = true;
-              shortcut = "1";
-              variant = "default";
-            }
-            {
-              action = "suspend";
-              enabled = true;
-              shortcut = "2";
-              variant = "default";
-            }
-            {
-              action = "reboot";
-              enabled = true;
-              shortcut = "3";
-              variant = "default";
-            }
-            {
-              action = "shutdown";
-              enabled = true;
-              shortcut = "4";
-              variant = "destructive";
-            }
-          ];
+              timeout = 900.0;
+          };
         };
       };
-      control_center = {
-        sidebar = "full";
+      wallpaper = {
+        directory = "/home/muhammadtalha/Pictures";
+        fill_mode = "stretch";
+        default.path = "/home/muhammadtalha/Pictures/buildings.png";
+        default.last = "/home/muhammadtalha/Pictures/buildings.png";
+        default.monitors."${outputMonitor}".path = "/home/muhammadtalha/Pictures/buildings.png";
       };
-      keybinds = {
-        cancel = [ "Escape" ];
-        validate = [ "Return" ];
+      plugins.enabled = [
+        "noctalia/screen_recorder"
+      ];
+      plugin_settings."noctalia/screen_recorder" = {
+        color_range = "full";
+        quality = "high";
+        video_source = "focused";
       };
-      bar = {
-        order = [ "main" ];
-        main = {
-          enabled = true;
-          background_opacity = 0.5;
-          capsule = true;
-          start = [
-            "workspaces"
-            "active_window"
-          ];
-          center = [
-            "notifications"
-            "clock"
-          ];
-          end = [
-            "network"
-            "bluetooth"
-            "volume"
-            "brightness"
-            "battery"
-          ];
-          font_weight = 700;
-          margin_edge = 8.0;
-          margin_ends = 19.0;
-          padding = 15;
-          thickness = 39;
-          widget_spacing = 7  ;
-        };
-      };
-      widgets = {
-        active_window = {
-          title_scroll = "on_hover";
-        };
-
-        audio_visualizer = {
-          capsule = true;
-          show_when_idle = false;
-        };
-
-        clock = {
-          format = "{:%-I:%M %p}";
-        };
-
-        "control-center" = {
-          capsule = true;
-          capsule_opacity = 1;
-          capsule_radius = 9.0;
-        };
-
-        date = {
-          format = "{:%b %d, %Y}";
-        };
-
-        gap = {
-          length = 22.0;
-          type = "spacer";
-        };
-
-        media = {
-          capsule = true;
-        };
-      };
-      desktop_widgets = {
-        enabled = false;
-        schema_version = 1;
-        widget_order = [];
-        grid = {
-          cell_size = 16;
-          major_interval = 4;
-          visible = true;
-        };
-      };
+      widget.recorder.type = "noctalia/screen_recorder:recorder";
       theme = {
         builtin = "Gruvbox";
-        mode = "dark";
-        source = "builtin";
         templates = {
-          builtin_ids = [
+          builtin_ids = [ 
             "gtk3"
             "gtk4"
-            "hyprland"
             "kcolorscheme"
             "qt"
           ];
           enable_community_templates = false;
         };
       };
-      wallpaper = {
-        directory = "/home/muhammadtalha/Pictures";
-        fill_mode = "stretch";
+      control_center = {
+        sidebar = "full";
+        sidebar_section = "full";
+        shortcuts = [
+          {
+            type = "wifi";  
+          }
+          {
+            type = "bluetooth";
+          }
+          {
+            type = "power_profile";
+          }
+          {
+            type = "clipboard";
+          }
+        ];
       };
-      weather = {
-        address = "Mardan, Pakistan";
+      bar = {
+        order = [ "main" ];
+        main = {
+          background_opacity = 0.0;
+          start = [ "group:g2" "group:g1" ];
+          center = [ "group:g8" "group:g3" "group:g4" ];
+          end = [ "group:g9" "group:g6" "group:g5" "group:g7" ];
+          font_weight = 700;
+          margin_edge = 7;
+          margin_ends = 1;
+          radius = 0;
+          scale = 1.1499999999999999;
+          thickness = 40;
+          widget_spacing = 5;
+          capsule_group = [
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g1";
+              members = [ "active_window" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g2";
+              members = [ "workspaces" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g3";
+              members = [ "notifications" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g4";
+              members = [ "clock" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g5";
+              members = [ "network" "bluetooth" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g6";
+              members = [ "brightness" "output_volume" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g7";
+              members = [ "battery" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g8";
+              members = [ "weather" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+            {
+              border = "outline";
+              fill = "outline";
+              foreground = "on_surface";
+              id = "g9";
+              members = [ "screenshot" "recorder" ];
+              opacity = 1.0;
+              padding = 6.0;
+              radius = 5.0;
+            }
+          ];
+        };
+      };
+      location.address = "Mardan, Pakistan";
+      weather.refresh_minutes = 5;
+      desktop_widgets.enabled = false;
+      lockscreen.blurred_desktop = true;
+      lockscreen_widgets = {
+        enabled = true;
+        schema_version = 2;
+        widget_order = [
+          "loginbox"
+          "audiovis"
+          "time"
+          "date"
+        ];
+        grid = {
+          cell_size = 16;
+          major_interval = 4;
+          visible = true;
+        };
+        widget.loginbox = {
+          box_height = 64.369140625;
+          box_width = 373.45703125;
+          cx = 683.267578125;
+          cy = 499.9794921875;
+          output = outputMonitor;
+          rotation = 0.0;
+          type = "login_box";
+          settings = {
+            background_color = "surface_variant";
+            background_opacity = 0.0;
+            background_radius = 0.0;
+            input_opacity = 0.0;
+            input_radius = 0.0;
+            show_login_button = false;
+          };
+        };
+        widget.audiovis = {
+          box_height = 35.06640625;
+          box_width = 356.1640625;
+          cx = 690.44140625;
+          cy = 557.400390625;
+          output = outputMonitor;
+          rotation = 0.0;
+          type = "audio_visualizer";
+          settings = {
+            background = false;
+            bands = 100;
+            centered = false;
+            color_1 = "primary";
+            color_2 = "secondary";
+            show_when_idle = false;
+          };
+        };
+        widget.time = {
+          box_height = 144.125;
+          box_width = 318.408203125;
+          cx = 683.0;
+          cy = 192.0;
+          output = outputMonitor;
+          rotation = 0.0;
+          type = "clock";
+          settings = {
+            background = false;
+            color = "secondary";
+            format = "{:%H:%M}";
+          };
+        };
+        widget.date = {
+          box_height = 48.0;
+          box_width = 288.0;
+          cx = 683.0;
+          cy = 264.0625;
+          output = outputMonitor;
+          rotation = 0.0;
+          type = "clock";
+          settings = {
+            background = false;
+            color = "secondary";
+            format = "{:%A, %d %B}";
+          };
+        };
       };
     };  
   };
