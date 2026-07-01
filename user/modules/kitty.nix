b00301b5e5bd1db575f8b134e0d44a56ee8a9a23
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-01 22:21:32 +0500

    add multiplexing keybinds

--- user/modules/kitty.nix
+++ user/modules/kitty.nix
@@ -11,6 +11,7 @@
     enable_audio_bell = false;
     scrollback = "never";
     scrollback_lines = 100000;
+    enabled_layouts  = "splits,stack";
   };
   programs.kitty.themeFile =  "GruvboxMaterialDarkHard";
   programs.kitty.enableGitIntegration = true;
@@ -25,6 +26,17 @@
     "ctrl+shift+q" = "close_tab";
     "ctrl+shift+left" = "previous_tab";
     "ctrl+shift+right" = "next_tab";
+    "ctrl+shift+enter" = "launch --location=vsplit --cwd=current";
+    "ctrl+alt+enter" = "launch --location=hsplit --cwd=current";
+    "ctrl+alt+left" =  "resize_window narrower 2";
+    "ctrl+alt+right" =  "resize_window wider 2";
+    "ctrl+alt+up" =  "resize_window taller 2";
+    "ctrl+alt+down" =  "resize_window shorter 2";
+    "ctrl+shift+home" =  "resize_window reset";
+    "ctrl+left" =  "neighboring_window left";
+    "ctrl+right" =  "neighboring_window right";
+    "ctrl+up" =  "neighboring_window up";
+    "ctrl+down" =  "neighboring_window down";
     "ctrl+shift+equal" = "change_font_size all +2.0";
     "ctrl+shift+minus" = "change_font_size all -2.0";
     "ctrl+shift+backspace" = "change_font_size all 0";
