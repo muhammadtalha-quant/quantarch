2d8dac8e8b8efbb2d51c75e7c131348641b85380
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-05 11:27:39 +0500

    formatted for nvim

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
-  description = "A monolithic single host flake that manages my complete NixOS System";
+    description = "A monolithic single host flake that manages my complete NixOS System";
 
   
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    homeManager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        homeManager = {
+        url = "github:nix-community/home-manager/master";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
-      url = "github:nix-community/disko/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+        url = "github:nix-community/disko/master";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
     	url = "github:noctalia-dev/noctalia";
@@ -21,29 +21,29 @@
 	    inputs.nixpkgs.follows = "nixpkgs";
     };
     nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
+        url = "github:notashelf/nvf";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
+};
 
-  outputs = { self, nixpkgs , homeManager, disko, ... } @inputs : {
-    nixosConfigurations.quantnix = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./system/configuration.nix
-        homeManager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.muhammadtalha = import ./user/home.nix;
-            backupFileExtension = "bak";
-            extraSpecialArgs = { inherit inputs; };
-          };
-        }
-        disko.nixosModules.disko
-        ./system/disko.nix
-      ];
+    outputs = { self, nixpkgs , homeManager, disko, ... } @inputs : {
+        nixosConfigurations.quantnix = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs; };
+            modules = [
+                ./system/configuration.nix
+                homeManager.nixosModules.home-manager
+                {
+                home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.muhammadtalha = import ./user/home.nix;
+                    backupFileExtension = "bak";
+                    extraSpecialArgs = { inherit inputs; };
+                  };
+                }
+                disko.nixosModules.disko
+                ./system/disko.nix
+            ];
+        };
     };
-  };
 }
--- system/modules/services.nix
+++ system/modules/services.nix
@@ -1,5 +1,5 @@
 {...}: {
-  services.power-profiles-daemon.enable = true;
-  services.upower.enable = true;
-  services.udisks2.enable = true;
+    services.power-profiles-daemon.enable = true;
+    services.upower.enable = true;
+    services.udisks2.enable = true;
 }
