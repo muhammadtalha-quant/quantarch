3bbfa5209f6f5e44c48c2e6a07d0c951209f637b
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-07 16:30:00 +0500

    set both editor types to nvim

--- user/modules/fish.nix
+++ user/modules/fish.nix
@@ -65,7 +65,7 @@ in {
     set -U fish_greeting
     set -gx LANG en_US.UTF-8
     set -gx EDITOR nvim
-    set -gx VISUAL nano
+    set -gx VISUAL nvim
     set -gx FLAKE_PATH /home/muhammadtalha/dotnix
     fish_add_path $HOME/.local/bin
   '';
