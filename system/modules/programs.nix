429b6ef5020c6156045a65406ab80e08c8403c6e
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-10 12:16:33 +0500

    add settings to cache passphrase

--- system/modules/programs.nix
+++ system/modules/programs.nix
@@ -1,17 +1,21 @@
-{pkgs, inputs,  ...}: {
-  programs.sway = {
-    enable = true;
-    package = pkgs.sway;
-    wrapperFeatures.gtk = true;
-    xwayland.enable = true;
-    extraPackages = [ ];
-  };
-  programs.fish.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-curses;
-    enableSSHSupport = true;
-  }; 
-  programs.gpu-screen-recorder.enable = true;
-  programs.dconf.enable = true;
+{ pkgs, inputs, ... }: {
+    programs.sway = {
+        enable = true;
+        package = pkgs.sway;
+        wrapperFeatures.gtk = true;
+        xwayland.enable = true;
+        extraPackages = [ ];
+    };
+    programs.fish.enable = true;
+    programs.gnupg.agent = {
+        enable = true;
+        pinentryPackage = pkgs.pinentry-gnome3;
+        enableSSHSupport = true;
+        settings = {
+            default-cache-ttl = 28800;
+            max-cache-ttl = 28800;
+        };
+    };
+    programs.gpu-screen-recorder.enable = true;
+    programs.dconf.enable = true;
 }
