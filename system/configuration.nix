f47f3be4521fa0b06fde50d1e57a88deef931965
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-10 11:53:58 +0500

    add session variables; remove fish variables

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,31 +1,35 @@
-{config, inputs, ... }:
+{ config, inputs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ./modules/hardware.nix
-    ./modules/audio.nix
-    ./modules/services.nix
-    ./modules/users.nix
-    ./modules/bootloader.nix
-    ./modules/programs.nix
-    ./modules/packages.nix
-    ./modules/networking.nix
-    ./modules/firewall.nix
-    ./modules/i18n.nix
-    ./modules/nix.nix
-    ./modules/nh.nix
-    ./modules/xdg.nix
-  ];
-  
-  environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "iHD";
-    QT_QPA_PLATFORMTHEME = "gtk3";
-    QT_QPA_PLATFORM = "wayland;xcb";
-    GTK_THEME = "Gruvbox-Dark";
-  };
-  time.timeZone = "Asia/Karachi";
-  nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "26.05";
+    imports = [
+        ./hardware-configuration.nix
+        ./modules/hardware.nix
+        ./modules/audio.nix
+        ./modules/services.nix
+        ./modules/users.nix
+        ./modules/bootloader.nix
+        ./modules/programs.nix
+        ./modules/packages.nix
+        ./modules/networking.nix
+        ./modules/firewall.nix
+        ./modules/i18n.nix
+        ./modules/nix.nix
+        ./modules/nh.nix
+        ./modules/xdg.nix
+    ];
+
+    environment.sessionVariables = {
+        LIBVA_DRIVER_NAME = "iHD";
+        QT_QPA_PLATFORMTHEME = "gtk3";
+        QT_QPA_PLATFORM = "wayland;xcb";
+        GTK_THEME = "Gruvbox-Dark";
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+        FLAKE_PATH = "/home/muhammadtalha/dotnix";
+        LANG = "en_US.UTF-8";
+    };
+    time.timeZone = "Asia/Karachi";
+    nixpkgs.config.allowUnfree = true;
+    system.stateVersion = "26.05";
 
 }
--- user/modules/fish.nix
+++ user/modules/fish.nix
@@ -1,79 +1,77 @@
-{pkgs, ...}: let
-  myAbbreviations = {
-    ls = "eza --icons --color=always";
-    lla = "eza -lgaoh --icons --git";
-    ll = "eza -lgoh --icons --git";
-    lf = "eza -goh --icons --only-files --show-symlinks --git";
-    ldir = "eza -goh --icons --only-dirs --show-symlinks --git";
-    laf = "eza -gaoh --icons --only-files --show-symlinks --git";
-    ladir = "eza -gaoh --icons --only-dirs --show-symlinks --git";
-    llaf = "eza -lgaoh --icons --only-files --show-symlinks --git";
-    lladir = "eza -lgaoh --icons --only-dirs --show-symlinks --git";
-    llf = "eza -lgoh --icons --only-files --show-symlinks --git";
-    lldir = "eza -lgoh --icons --only-dirs --show-symlinks --git";
-    la = "eza -ah --icons --color=always";
-    lt = "eza --tree --icons --git";
-    home = "cd ~";
-    ".." = "cd ..";
-    "..." = "cd ../..";
-    "...." = "cd ../../..";
-    "000" = "chmod 000";
-    "644" = "chmod 644";
-    "666" = "chmod 666";
-    "755" = "chmod 755";
-    "777" = "chmod 777";
-    "000r" = "chmod -R 000";
-    "644r" = "chmod -R 644";
-    "666r" = "chmod -R 666";
-    "755r" = "chmod -R 755";
-    "777r" = "chmod -R 777";
-    snano = "sudo nano";
-    mkdir = "mkdir -p";
-    cp = "cp -rv";
-    mv = "mv -v";
-    rm = "rm -frv";
-    vi = "nvim";
-    lg = "lazygit";
-    sucp = "sudo cp -rv";
-    sumv = "sudo mv -v";
-    surm = "sudo rm -frv";
-    less = "less -R";
-    cls = "clear";
-    vim = "nvim";
-    s = "nh os switch";
-    b = "nh os boot";
-    t = "nh os test";
-    u = "nix flake update --flake $FLAKE_PATH";
-    fm = "yazi";
-  };
-  myPlugins = [
-    {
-      name = "done";
-      src = pkgs.fishPlugins.done.src;
-    }
-    {
-      name = "autopair";
-      src = pkgs.fishPlugins.autopair.src;
-    }
-  ];
-  clh = ''
-    echo yes | history clear
-    clear
-  '';
-in {
-  xdg.configFile."fish/conf.d/shellVars.fish".text = ''
-    set -U fish_greeting
-    set -gx LANG en_US.UTF-8
-    set -gx EDITOR nvim
-    set -gx VISUAL nvim
-    set -gx FLAKE_PATH /home/muhammadtalha/dotnix
-    fish_add_path $HOME/.local/bin
-  '';
-  programs.fish = {
-    enable = true;
-    shellAbbrs = myAbbreviations;
-    shellInit = "fastfetch";
-    plugins = myPlugins;
-    functions.clh.body = clh;
-  };
+{ pkgs, ... }:
+let
+    myAbbreviations = {
+        ls = "eza --icons --color=always";
+        lla = "eza -lgaoh --icons --git";
+        ll = "eza -lgoh --icons --git";
+        lf = "eza -goh --icons --only-files --show-symlinks --git";
+        ldir = "eza -goh --icons --only-dirs --show-symlinks --git";
+        laf = "eza -gaoh --icons --only-files --show-symlinks --git";
+        ladir = "eza -gaoh --icons --only-dirs --show-symlinks --git";
+        llaf = "eza -lgaoh --icons --only-files --show-symlinks --git";
+        lladir = "eza -lgaoh --icons --only-dirs --show-symlinks --git";
+        llf = "eza -lgoh --icons --only-files --show-symlinks --git";
+        lldir = "eza -lgoh --icons --only-dirs --show-symlinks --git";
+        la = "eza -ah --icons --color=always";
+        lt = "eza --tree --icons --git";
+        home = "cd ~";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        "...." = "cd ../../..";
+        "000" = "chmod 000";
+        "644" = "chmod 644";
+        "666" = "chmod 666";
+        "755" = "chmod 755";
+        "777" = "chmod 777";
+        "000r" = "chmod -R 000";
+        "644r" = "chmod -R 644";
+        "666r" = "chmod -R 666";
+        "755r" = "chmod -R 755";
+        "777r" = "chmod -R 777";
+        snano = "sudo nano";
+        mkdir = "mkdir -p";
+        cp = "cp -rv";
+        mv = "mv -v";
+        rm = "rm -frv";
+        vi = "nvim";
+        lg = "lazygit";
+        sucp = "sudo cp -rv";
+        sumv = "sudo mv -v";
+        surm = "sudo rm -frv";
+        less = "less -R";
+        cls = "clear";
+        vim = "nvim";
+        s = "nh os switch";
+        b = "nh os boot";
+        t = "nh os test";
+        u = "nix flake update --flake $FLAKE_PATH";
+        fm = "yazi";
+    };
+    myPlugins = [
+        {
+            name = "done";
+            src = pkgs.fishPlugins.done.src;
+        }
+        {
+            name = "autopair";
+            src = pkgs.fishPlugins.autopair.src;
+        }
+    ];
+    clh = ''
+        clear && echo yes | history clear
+        fish
+    '';
+in
+{
+    xdg.configFile."fish/conf.d/fishenv.fish".text = ''
+        set -U fish_greeting 
+        fish_add_path $HOME/.local/bin
+    '';
+    programs.fish = {
+        enable = true;
+        shellAbbrs = myAbbreviations;
+        shellInit = "fastfetch";
+        plugins = myPlugins;
+        functions.clh.body = clh;
+    };
 }
