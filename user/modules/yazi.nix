cd20c26ec16745560d86aa0119aed31f69ce1309
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-16 14:19:14 +0500

    add some plugins; remove flavour

--- user/modules/yazi.nix
+++ user/modules/yazi.nix
@@ -1,56 +1,46 @@
-{pkgs, ...}: {
-  programs.yazi = {
-    enable = true;
-    enableFishIntegration = true;
-    plugins = {
-      ouch = pkgs.yaziPlugins.ouch;
-    };    
-    flavors = {
-      gruvbox = pkgs.fetchFromGitHub {
-        owner = "bennyyip";
-        repo = "gruvbox-dark.yazi";
-        rev = "619fdc5844db0c04f6115a62cf218e707de2821e";
-        sha256 = "sha256-Y/i+eS04T2+Sg/Z7/CGbuQHo5jxewXIgORTQm25uQb4=";
-      };
+{ pkgs, ... }: {
+    programs.yazi = {
+        enable = true;
+        enableFishIntegration = true;
+        plugins = {
+            ouch = pkgs.yaziPlugins.ouch;
+            smart_enter = pkgs.yaziPlugins.smart-enter;
+            yatline = pkgs.yaziPlugins.yatline;
+            recycleBin = pkgs.yaziPlugins.omni-trash;
+        };
+        settings = {
+            mgr = {
+                sort_by = "mtime";
+                sort_sensitive = false;
+                sort_dir_first = true;
+                linemode = "permissions";
+                show_hidden = false;
+                show_symlink = true;
+            };
+            preview.wrap = "no";
+            plugin.prepend_previewers = [
+                {
+                    mime = "application/{*zip,tar,bzip2,7z*,rar,xz,zstd,java-archive}";
+                    run = "ouch";
+                }
+            ];
+            opener = {
+                extract = [
+                    {
+                        run = "ouch d -y \"$@\"";
+                        desc = "Extract here with ouch";
+                        for = "unix";
+                    }
+                ];
+            };
+        };
+        keymap = {
+            mgr.prepend_keymap = [
+                {
+                    on = [ "C" ];
+                    run = "plugin ouch";
+                }
+            ];
+        };
     };
-    theme = {
-      flavor = {
-        dark = "gruvbox";
-      };
-    };
-    settings = {
-      mgr = {
-        sort_by = "mtime";
-        sort_sensitive = false;
-        sort_dir_first = true;
-        linemode = "permissions";
-        show_hidden = false;
-        show_symlink = true;
-      };
-      preview.wrap = "no";
-      plugin.prepend_previewers = [
-        {
-          mime = "application/{*zip,tar,bzip2,7z*,rar,xz,zstd,java-archive}";
-          run  = "ouch";
-        }
-      ];
-      opener = {
-        extract = [
-          {
-            run = "ouch d -y \"$@\"";
-            desc = "Extract here with ouch";
-            for = "unix";
-          }
-        ];
-      };
-    };
-    keymap = {
-      mgr.prepend_keymap = [
-        {
-          on   = [ "C" ];
-          run  = "plugin ouch";
-        }
-      ];
-    };
-  }; 
 }
