43c3830d11a1ffb69e5c62a212a93abde90cd0f7
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-16 14:20:01 +0500

    replace gruvbox colorscheme with catppuccin macchiato

--- system/modules/stylix.nix
+++ system/modules/stylix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
     stylix = {
         enable = true;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
         polarity = "dark";
         fonts = {
             serif = {
