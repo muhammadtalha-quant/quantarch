8c98263cf011666708aa87be1f4697a4f5b9d2e8
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-16 21:49:28 +0500

    add xdp for niri

--- system/modules/xdg.nix
+++ system/modules/xdg.nix
@@ -1,18 +1,28 @@
-{pkgs, ...}: {
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
-  xdg.mime = {
-    enable = true;
-    defaultApplications = {
-      "inode/directory" = [ "yazi.desktop" ];
-      "text/*" = [ "nvim.desktop" ];
-      "application/pdf" = [ "org.pwmt.zathura.desktop" ];
-      "image/*" = [ "imv.desktop" ];
-      "video/*" = [ "umpv.desktop" ];
-      "audio/*" = [ "umpv.desktop" ];
+{ pkgs, ... }: {
+    xdg.portal = {
+        enable = true;
+        extraPortals = [
+            pkgs.xdg-desktop-portal-gtk
+            pkgs.xdg-desktop-portal-gnome
+        ];
+        config = {
+            common = {
+                defualt = [
+                    "gnome"
+                    "gtk"
+                ];
+            };
+        };
     };
-  };
+    xdg.mime = {
+        enable = true;
+        defaultApplications = {
+            "inode/directory" = [ "yazi.desktop" ];
+            "text/*" = [ "nvim.desktop" ];
+            "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+            "image/*" = [ "imv.desktop" ];
+            "video/*" = [ "umpv.desktop" ];
+            "audio/*" = [ "umpv.desktop" ];
+        };
+    };
 }
