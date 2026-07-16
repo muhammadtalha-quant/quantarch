e791de6adcd83c348273591518fc56adc3c38884
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-16 14:19:04 +0500

    remove color/theme injection

--- user/modules/cava.nix
+++ user/modules/cava.nix
@@ -1,31 +1,19 @@
-{...}: {
-  programs.cava = {
-    enable = true;
-    settings = {
-      general.framerate = 60;
-      general.bars = 0; # fill available space
-      general.bar_width = 2;
-      general.bar_spacing = 1;
-      general.max_height = 100; # total height of terminal
-      output.method = "noncurses";
-      output.orientation = "bottom";
-      color.theme = "noctalia";
+{ ... }: {
+    programs.cava = {
+        enable = true;
+        settings = {
+            general = {
+                framerate = 60;
+                bars = 0; # fill available space
+                bar_width = 2;
+                bar_spacing = 1;
+                max_height = 100; # total height of terminal
+            };
+            output = {
+                method = "noncurses";
+                orientation = "bottom";
+            };
+        };
     };
-  };
-  xdg.configFile."cava/themes/noctalia".text = ''
-    [color]
-    foreground = '#b8bb26'
-    
-    gradient = 1
-    gradient_color_1 = '#444507'
-    gradient_color_2 = '#b8bb26'
-    gradient_color_3 = '#f6f6d5'
-    
-    horizontal_gradient = 0
-    horizontal_gradient_color_1 = '#444507'
-    horizontal_gradient_color_2 = '#b8bb26'
-    horizontal_gradient_color_3 = '#f6f6d5'
-    horizontal_gradient_color_4 = '#b8bb26'
-    horizontal_gradient_color_5 = '#444507'
-  '';
 }
+
