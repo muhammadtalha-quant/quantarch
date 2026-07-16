b24a40a2e887f82eb7194684d357a53be17bf480
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-16 19:18:13 +0500

    remove presets and overrides from starship

--- user/modules/starship.nix
+++ user/modules/starship.nix
@@ -1,12 +1,5 @@
 {
-    stylix.targets.starship.enable = false;
     programs.starship = {
         enable = true;
-        presets = [ "catppuccin-powerline" ];
-        # custom overrides
-        settings = {
-            os.symbols.NixOS = " ";
-            palette = "catppuccin_macchiato";
-        };
     };
 }
