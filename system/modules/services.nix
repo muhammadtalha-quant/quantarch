eee38958a0aa55730fcbae948d4a66521b1619bf
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-17 21:05:08 +0500

    remove audio.nix; add module security.nix
    audio.nix merged into services.nix

--- system/modules/services.nix
+++ system/modules/services.nix
@@ -1,8 +1,17 @@
-{ ... }: {
+{
     services = {
         power-profiles-daemon.enable = true;
         upower.enable = true;
+        # Uncomment to enable fingerprint login support.
+        #fprintd.enable = true;
         udisks2.enable = true;
+        pulseaudio.enable = false;
+        pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+        };
         displayManager.dms-greeter = {
             enable = true;
             compositor.name = "niri";
