765fd5fbdbd890f67314fc6acfc75dd702097537
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-17 22:54:02 +0500

    found the best gruvbox

--- system/modules/stylix.nix
+++ system/modules/stylix.nix
@@ -2,7 +2,7 @@
     stylix = {
         overlays.enable = false;
         enable = true;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
         polarity = "dark";
         fonts = {
             serif = {
