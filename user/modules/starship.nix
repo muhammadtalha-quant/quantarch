41be8040c0f6fbbb1aa26d152c29c6bbdab701d5
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-20 14:08:39 +0500

    cofigured module starship

--- user/modules/starship.nix
+++ user/modules/starship.nix
@@ -5,29 +5,8 @@
         enable = true;
         enableFishIntegration = true;
         settings = {
-            format = ''
-                [](base09)\
-                $os\
-                $username\
-                [](bg:base0A fg:base09)\
-                $directory\
-                [](fg:base0A bg:base0C)\
-                $git_branch\
-                $git_status\
-                [](fg:base0C bg:base0D)\
-                $typst\
-                $cmake\
-                $c\
-                $cpp\
-                $python\
-                [](fg:base0D bg:base03)\
-                $docker_context\
-                $conda\
-                [](fg:base03 bg:base01)\
-                $time\
-                [ ](fg:base01)\
-                $line_break$character
-            '';
+            "$schema" = "https://starship.rs/config-schema.json";
+            format = "[](base09)$os$username[](bg:base0a fg:base09)$directory[](fg:base0a bg:base0c)$git_branch$git_status[](fg:base0c bg:base0d)$typst$cmake$c$cpp$python[](fg:base0d bg:base01)$time[ ](fg:base01)$line_break$character";
             line_break.disabled = false;
             os = {
                 disabled = false;
@@ -42,7 +21,7 @@
                 format = "[ $user ]($style)";
             };
             directory = {
-                style = "fg:base07 bg:base0A";
+                style = "fg:base07 bg:base0a";
                 format = "[ $path ]($style)";
                 truncation_length = 3;
                 truncation_symbol = "…/";
@@ -54,53 +33,53 @@
                     "Development" = "󰲋 ";
                     "development" = "󰲋 ";
                 };
-                git_branch = {
-                    symbol = " ";
-                    style = "bg:base0C";
-                    format = "[[ $symbol $branch ](fg:base07 bg:base0C)]($style)";
-                };
-                git_status = {
-                    style = "bg:base0C";
-                    format = "[[($all_status$ahead_behind )](fg:base07 bg:base0C)]($style)";
-                };
-                c = {
-                    symbol = " ";
-                    style = "bg:base0D";
-                    format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
-                };
-                cmake = {
-                    symbol = " ";
-                    style = "bg:base0D";
-                    format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
-                };
-                cpp = {
-                    symbol = " ";
-                    style = "bg:base0D";
-                    format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
-                };
-                python = {
-                    symbol = "󰌠 ";
-                    style = "bg:base0D";
-                    format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
-                };
-                typst = {
-                    symbol = " ";
-                    style = "bg:base0D";
-                    format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
-                };
-                time = {
-                    disabled = false;
-                    time_format = "%R";
-                    style = "bg:base01";
-                    format = "[[  $time ](fg:base07 bg:base01)]($style)";
-                };
-                character = {
-                    disabled = false;
-                    success_symbol = "[](bold fg:base0B)";
-                    error_symbol = "[](bold fg:base08)";
-                };
             };
-            palatte = "gruvbox_forest";
+            git_branch = {
+                symbol = " ";
+                style = "bg:base0c";
+                format = "[[ $symbol $branch ](fg:base07 bg:base0c)]($style)";
+            };
+            git_status = {
+                style = "bg:base0c";
+                format = "[[($all_status$ahead_behind )](fg:base07 bg:base0c)]($style)";
+            };
+            c = {
+                symbol = " ";
+                style = "bg:base0d";
+                format = "[[ $symbol( $version) ](fg:base07 bg:base0d)]($style)";
+            };
+            cmake = {
+                symbol = " ";
+                style = "bg:base0d";
+                format = "[[ $symbol( $version) ](fg:base07 bg:base0d)]($style)";
+            };
+            cpp = {
+                symbol = " ";
+                style = "bg:base0d";
+                format = "[[ $symbol( $version) ](fg:base07 bg:base0d)]($style)";
+            };
+            python = {
+                symbol = "󰌠 ";
+                style = "bg:base0d";
+                format = "[[ $symbol( $version) ](fg:base07 bg:base0d)]($style)";
+            };
+            typst = {
+                symbol = " ";
+                style = "bg:base0d";
+                format = "[[ $symbol( $version) ](fg:base07 bg:base0d)]($style)";
+            };
+            time = {
+                disabled = false;
+                time_format = "%R";
+                style = "bg:base01";
+                format = "[[  $time ](fg:base07 bg:base01)]($style)";
+            };
+            character = {
+                disabled = false;
+                success_symbol = "[](bold fg:base0b)";
+                error_symbol = "[](bold fg:base08)";
+            };
+            palette = "gruvbox_forest";
             palettes.gruvbox_forest = {
                 base00 = "#282828";
                 base01 = "#3c3836";
@@ -112,13 +91,108 @@
                 base07 = "#fbf1c7";
                 base08 = "#fb4934";
                 base09 = "#fe8019";
-                base0A = "#fabd2f";
-                base0B = "#b8bb26";
-                base0C = "#5f6c1f";
-                base0D = "#b8bb26";
-                base0E = "#96bda2";
-                base0F = "#d65d0e";
+                base0a = "#fabd2f";
+                base0b = "#b8bb26";
+                base0c = "#5f6c1f";
+                base0d = "#b8bb26";
+                base0e = "#96bda2";
+                base0f = "#d65d0e";
             };
         };
+        #        settings = {
+        #            "$schema" = "https://starship.rs/config-schema.json";
+        #            format = "[](base09)$os$username[](bg:base0A fg:base09)$directory[](fg:base0A bg:base0C)$git_branch$git_status[](fg:base0C bg:base0D)$typst$cmake$c$cpp$python[](fg:base0D bg:base01)$time[ ](fg:base01)$line_break$character";
+        #            line_break.disabled = false;
+        #            os = {
+        #                disabled = false;
+        #                style = "bg:base09 fg:base07";
+        #                symbols.NixOS = " ";
+        #            };
+        #            username = {
+        #                disabled = false;
+        #                show_always = true;
+        #                style_user = "bg:base09 fg:base07";
+        #                style_root = "bg:base09 fg:base07";
+        #                format = "[ $user ]($style)";
+        #            };
+        #            directory = {
+        #                style = "fg:base07 bg:base0A";
+        #                format = "[ $path ]($style)";
+        #                truncation_length = 3;
+        #                truncation_symbol = "…/";
+        #                substitutions = {
+        #                    "Documents" = "󰈙 ";
+        #                    "Downloads" = " ";
+        #                    "Music" = "󰝚 ";
+        #                    "Pictures" = " ";
+        #                    "Development" = "󰲋 ";
+        #                    "development" = "󰲋 ";
+        #                };
+        #            };
+        #            git_branch = {
+        #                symbol = " ";
+        #                style = "bg:base0C";
+        #                format = "[[ $symbol $branch ](fg:base07 bg:base0C)]($style)";
+        #            };
+        #            git_status = {
+        #                style = "bg:base0C";
+        #                format = "[[($all_status$ahead_behind )](fg:base07 bg:base0C)]($style)";
+        #            };
+        #            c = {
+        #                symbol = " ";
+        #                style = "bg:base0D";
+        #                format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
+        #            };
+        #            cmake = {
+        #                symbol = " ";
+        #                style = "bg:base0D";
+        #                format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
+        #            };
+        #            cpp = {
+        #                symbol = " ";
+        #                style = "bg:base0D";
+        #                format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
+        #            };
+        #            python = {
+        #                symbol = "󰌠 ";
+        #                style = "bg:base0D";
+        #                format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
+        #            };
+        #            typst = {
+        #                symbol = " ";
+        #                style = "bg:base0D";
+        #                format = "[[ $symbol( $version) ](fg:base07 bg:base0D)]($style)";
+        #            };
+        #            time = {
+        #                disabled = false;
+        #                time_format = "%R";
+        #                style = "bg:base01";
+        #                format = "[[  $time ](fg:base07 bg:base01)]($style)";
+        #            };
+        #            character = {
+        #                disabled = false;
+        #                success_symbol = "[](bold fg:base0B)";
+        #                error_symbol = "[](bold fg:base08)";
+        #            };
+        #            palette = "gruvbox_forest";
+        #            palettes.gruvbox_forest = {
+        #                base00 = "#282828";
+        #                base01 = "#3c3836";
+        #                base02 = "#504945";
+        #                base03 = "#665c50";
+        #                base04 = "#bdae93";
+        #                base05 = "#d5c4a1";
+        #                base06 = "#ebdbb2";
+        #                base07 = "#fbf1c7";
+        #                base08 = "#fb4934";
+        #                base09 = "#fe8019";
+        #                base0A = "#fabd2f";
+        #                base0B = "#b8bb26";
+        #                base0C = "#5f6c1f";
+        #                base0D = "#b8bb26";
+        #                base0E = "#96bda2";
+        #                base0F = "#d65d0e";
+        #            };
+        #        };
     };
 }
