7aa884e899d1338490ae7ce96b7abb7c8b71caf3
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-07-20 23:11:51 +0500

    updated hardware graphics drivers

--- system/modules/hardware.nix
+++ system/modules/hardware.nix
@@ -1,17 +1,17 @@
-{pkgs, ...}: {
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      intel-vaapi-driver 
-      vpl-gpu-rt
-    ];
-  };
-  # they are services, but they work with hardware
-  services.libinput.enable = true;
-  services.pcscd.enable = true;
+{ pkgs, ... }: {
+    hardware.bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+    };
+    hardware.graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+            intel-media-driver
+            vpl-gpu-rt
+            intel-media-sdk
+        ];
+    };
+    # they are services, but they work with hardware
+    services.libinput.enable = true;
+    services.pcscd.enable = true;
 }
